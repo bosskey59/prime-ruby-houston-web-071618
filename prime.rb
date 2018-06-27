d9da0164c55ee2177cc5809b0743bd4526b5f26e
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-27 13:23:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,8 +4,14 @@ def prime?(integer)
   range = (2...integer)
   if integer<1
   return FALSE
+end
   
-  range.each {|x| if integer%x==0 return FALSE  }
+  range.each do |x| 
+    if integer%x==0 
+      return FALSE
+    end
+  end
+  
   
   return TRUE
   
